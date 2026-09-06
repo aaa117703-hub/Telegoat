49e0dca5233da4355eae869e154d1976ba9abfb5
Aqeel Al Rowai <aaa117703@gmail.com>
2026-09-07 01:02:40 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://qzsteswrannqsrnlytzl.supabase.co';
+const SUPABASE_ANON_KEY = 'Sb_publishable_Qa5O7t1wbhPhnTLBtHEfQg_hYYNdxYJ';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
