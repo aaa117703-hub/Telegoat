bfb224771aa154916be3346658dddd4b2c5d9999
Aqeel Al Rowai <aaa117703@gmail.com>
2026-09-15 05:26:49 +0300

    Update standings.js

--- standings.js
+++ standings.js
@@ -1,36 +1,22 @@
 /* =========================================================
    standings.js
-   حساب الترتيب + عرض الجدول مع الأسهم
 ========================================================= */
 
-
-/* =========================================================
-   CALCULATE STANDINGS UP TO ROUND
-   حساب الترتيب من الجولة 1 حتى جولة معينة
-========================================================= */
 
 function calculateStandingsUpToRound(targetRound) {
 
     const calculated = {};
 
-    /* تهيئة كل الفرق بـ 0 */
-    for (
-        const teamKey in initialBasePoints
-    ) {
-
+    for (const teamKey in initialBasePoints) {
         calculated[teamKey] = {
             key: teamKey,
             gf: 0,
-            pts: 0
+            pts: 0,
+            form: []
         };
     }
 
-    /* المرور على كل الجولات من 1 حتى الهدف */
-    for (
-        let r = 1;
-        r <= targetRound;
-        r++
-    ) {
+    for (let r = 1; r <= targetRound; r++) {
 
         const roundMatches = matchweeks[r];
 
@@ -38,99 +24,91 @@ function calculateStandingsUpToRound(targetRound) {
             continue;
         }
 
-        roundMatches.forEach(
-            (match, idx) => {
+        roundMatches.forEach(function(match, idx) {
 
-                const hKey = match[0];
-                const aKey = match[1];
+            const hKey = match[0];
+            const aKey = match[1];
 
-                const hVal =
-                    scoresStorage[
-                        `r${r}_m${idx}_home`
-                    ];
+            const hVal = scoresStorage['r' + r + '_m' + idx + '_home'];
+            const aVal = scoresStorage['r' + r + '_m' + idx + '_away'];
 
-                const aVal =
-                    scoresStorage[
-                        `r${r}_m${idx}_away`
-                    ];
+            if (
+                hVal !== undefined &&
+                aVal !== undefined &&
+                hVal !== '' &&
+                aVal !== ''
+            ) {
 
-                /* فقط إذا كانت النتيجتان موجودتان */
-                if (
-                    hVal !== undefined &&
-                    aVal !== undefined &&
-                    hVal !== '' &&
-                    aVal !== ''
-                ) {
+                const hScore = parseFloat(hVal);
+                const aScore = parseFloat(aVal);
 
-                    const hScore =
-                        parseFloat(hVal);
+                if (Number.isNaN(hScore) || Number.isNaN(aScore)) {
+                    return;
+                }
 
-                    const aScore =
-                        parseFloat(aVal);
+                calculated[hKey].gf += hScore;
+                calculated[aKey].gf += aScore;
 
-                    if (
-                        Number.isNaN(hScore) ||
-                        Number.isNaN(aScore)
-                    ) {
-                        return;
-                    }
+                if (hScore > aScore) {
 
-                    /* إضافة الأهداف */
-                    calculated[hKey].gf += hScore;
-                    calculated[aKey].gf += aScore;
+                    calculated[hKey].pts += 3;
+                    calculated[hKey].form.push('W');
+                    calculated[aKey].form.push('L');
 
-                    /* حساب النقاط */
-                    if (hScore > aScore) {
+                } else if (aScore > hScore) {
 
-                        calculated[hKey].pts += 3;
+                    calculated[aKey].pts += 3;
+                    calculated[aKey].form.push('W');
+                    calculated[hKey].form.push('L');
 
-                    } else if (aScore > hScore) {
+                } else {
 
-                        calculated[aKey].pts += 3;
-
-                    } else {
-
-                        calculated[hKey].pts += 1;
-                        calculated[aKey].pts += 1;
-                    }
+                    calculated[hKey].pts += 1;
+                    calculated[aKey].pts += 1;
+                    calculated[hKey].form.push('D');
+                    calculated[aKey].form.push('D');
                 }
             }
-        );
+        });
     }
 
-    /* ترتيب: أولاً بالنقاط، ثم بالأهداف */
     return Object
         .values(calculated)
-        .sort(
-            (a, b) =>
-                b.pts - a.pts ||
-                b.gf - a.gf
-        );
+        .sort(function(a, b) {
+            return b.pts - a.pts || b.gf - a.gf;
+        });
 }
 
 
-/* =========================================================
-   CALCULATE STANDINGS - الترتيب الحالي
-========================================================= */
-
-function calculateStandings() {
-
-    return calculateStandingsUpToRound(
-        currentRound
-    );
+function calculateStandings {
+()    return calculateStandingsUpToRound(currentRound);
 }
 
 
-/* =========================================================
-   RENDER STANDINGS - عرض جدول الترتيب
-========================================================= */
+function renderForm(formArray) {
+
+    if (!formArray || formArray.length === 0) {
+        return '<span style="color:#ccc;">-</span>';
+    }
+
+    /* آخر 5 مباريات */
+    const last5 = formArray.slice(-5);
+
+    let html = '<div class="form-cell">';
+
+    last5.forEach(function(letter) {
+        html += '<span class="form-letter form-' + letter + '">' + letter + '</span>';
+    });
+
+    html += '</div>';
+
+    return html;
+}
+
 
 function renderStandings() {
 
-    const tbody =
-        document.getElementById(
-            'standingsBody'
-        );
+    const tbody = document.getElementById('standingsBody');
 
     if (!tbody) {
         return;
@@ -140,130 +118,81 @@ function renderStandings() {
 
     const sortedTeams = calculateStandings();
 
-    /* حساب ترتيب الجولة السابقة للأسهم */
     const prevRanks = {};
 
     if (currentRound >= 2) {
 
-        const prevSortedTeams =
-            calculateStandingsUpToRound(
-                currentRound - 1
-            );
+        const prevSortedTeams = calculateStandingsUpToRound(currentRound - 1);
 
-        prevSortedTeams.forEach(
-            (team, idx) => {
-
-                prevRanks[team.key] = idx + 1;
-            }
-        );
+        prevSortedTeams.forEach(function(team, idx) {
+            prevRanks[team.key] = idx + 1;
+        });
     }
 
-    sortedTeams.forEach(
-        (team, index) => {
+    sortedTeams.forEach(function(team, index) {
 
-            const teamInfo =
-                teamsMap[team.key] || {
-                    name: team.key,
-                    logo: ''
-                };
+        const teamInfo = teamsMap[team.key] || { name: team.key, logo: '' };
+        const position = index + 1;
 
-            const position = index + 1;
+        let rowClass = '';
 
-            /* لون الخط (أخضر للأول / أحمر للـ 17) */
-            let rowClass = '';
-
-            if (position === 1) {
-                rowClass = 'green-line';
-
-            } else if (position === 17) {
-                rowClass = 'red-line';
-            }
-
-            /* سهم الصعود/الهبوط/الثبات */
-            let indicatorHtml = '';
-
-            if (
-                currentRound >= 2 &&
-                prevRanks[team.key] !== undefined
-            ) {
-
-                const oldPos = prevRanks[team.key];
-
-                let imgSrc = '';
-
-                if (position < oldPos) {
-
-                    imgSrc = './arrow_up_green.png';
-
-                } else if (position > oldPos) {
-
-                    imgSrc = './arrow_down_red.png';
-
-                } else {
-
-                    imgSrc = './equal_gray.png';
-                }
-
-                indicatorHtml = `
-                    <img
-                        src="${imgSrc}"
-                        class="pos-indicator-img"
-                        alt="trend"
-                        onerror="
-                            this.style.display='none'
-                        "
-                    >
-                `;
-            }
-
-            /* إضافة الصف */
-            tbody.innerHTML += `
-                <tr class="${rowClass}">
-
-                    <td>
-                        <div class="pos-container">
-                            ${indicatorHtml}
-                            <span class="pos-number">
-                                ${position}
-                            </span>
-                        </div>
-                    </td>
-
-                    <td>
-                        <div class="team-cell">
-
-                            <span class="logo-20">
-                                <img
-                                    src="./${teamInfo.logo}"
-                                    alt="${teamInfo.name}"
-                                    onerror="
-                                        this.style.display='none'
-                                    "
-                                >
-                            </span>
-
-                            ${teamInfo.name}
-
-                        </div>
-                    </td>
-
-                    <td>
-                        <span class="stat-number">
-                            ${team.gf}
-                        </span>
-                    </td>
-
-                    <td>
-                        <span
-                            class="stat-number"
-                            style="color:#7138f5;"
-                        >
-                            ${team.pts}
-                        </span>
-                    </td>
-
-                </tr>
-            `;
+        if (position === 1) {
+            rowClass = 'pos-1';
+        } else if (position === 2) {
+            rowClass = 'pos-2';
+        } else if (position === 3) {
+            rowClass = 'pos-3';
         }
-    );
+
+        if (position === 1) {
+            rowClass += ' green-line';
+        } else if (position === 17) {
+            rowClass += ' red-line';
+        }
+
+        let indicatorHtml = '';
+
+        if (currentRound >= 2 && prevRanks[team.key] !== undefined) {
+
+            const oldPos = prevRanks[team.key];
+            let imgSrc = '';
+
+            if (position < oldPos) {
+                imgSrc = './arrow_up_green.png';
+            } else if (position > oldPos) {
+                imgSrc = './arrow_down_red.png';
+            } else {
+                imgSrc = './equal_gray.png';
+            }
+
+            indicatorHtml = '<img src="' + imgSrc + '" class="pos-indicator-img" alt="trend" onerror="this.style.display=\'none\'">';
+        }
+
+        tbody.innerHTML +=
+            '<tr class="' + rowClass + '">' +
+                '<td>' +
+                    '<div class="pos-container">' +
+                        indicatorHtml +
+                        '<span class="pos-number">' + position + '</span>' +
+                    '</div>' +
+                '</td>' +
+                '<td>' +
+                    '<div class="team-cell">' +
+                        '<span class="logo-20">' +
+                            '<img src="./' + teamInfo.logo + '" alt="' + teamInfo.name + '" onerror="this.style.display=\'none\'">' +
+                        '</span>' +
+                        teamInfo.name +
+                    '</div>' +
+                '</td>' +
+                '<td>' +
+                    '<span class="stat-number">' + team.gf + '</span>' +
+                '</td>' +
+                '<td>' +
+                    '<span class="stat-number" style="color:#7138f5;">' + team.pts + '</span>' +
+                '</td>' +
+                '<td>' +
+                    renderForm(team.form) +
+                '</td>' +
+            '</tr>';
+    });
 }
